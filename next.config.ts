b301fea2361011b31c4e388b5eea1b47d5e1b928
Semih Hasanoğlu <semih@Semih-MacBook-Pro.local>
2026-06-01 14:45:18 +0300

    fix(csp): remove upgrade-insecure-requests for local dev

--- next.config.ts
+++ next.config.ts
@@ -35,7 +35,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://va.vercel-scripts.com https://cal.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' blob: data: https:; font-src 'self' https://fonts.gstatic.com; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; frame-src 'self' https://cal.com; media-src 'self' https:; connect-src 'self' https://api.web3forms.com; upgrade-insecure-requests;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://va.vercel-scripts.com https://cal.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' blob: data: https:; font-src 'self' https://fonts.gstatic.com; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; frame-src 'self' https://cal.com; media-src 'self' https:; connect-src 'self' https://api.web3forms.com;",
           },
         ],
       },
